426fc9ae722dff0ce5ed983ad55af7d8eb747ade
Vidhya0501 <vidhyasuri97@gmail.com>
2024-07-27 03:04:48 +0530

    cors updated

--- real_estate_be/index.js
+++ real_estate_be/index.js
@@ -9,12 +9,7 @@ const authController = require("./controllers/authController.js");
 const propertyController = require("./controllers/propertyController.js");
 
 dotenv.config();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
